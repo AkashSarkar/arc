f886803398a873e96db87792645ea8be51f5b70b
Akash Sarkar <akashsarker1433@gmail.com>
2026-04-20 14:10:36 +0900

    Initial Commit

---
+++ Arc/ArcApp.swift
@@ -0,0 +1,17 @@
+//
+//  ArcApp.swift
+//  Arc
+//
+//  Created by Work on 4/20/26.
+//
+
+import SwiftUI
+
+@main
+struct ArcApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
